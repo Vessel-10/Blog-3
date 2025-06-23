6e2418cac4de2b7b163b8bcd48bf958c73f1ec95
vessel <magomboanaclet@gmail.com>
2025-06-23 13:16:26 +0200

    Don't know what i have done

---
+++ admin/pages/profile.php
@@ -0,0 +1,8 @@
+<div class="profile">
+    <div class="profile-img">
+        <img src="https://images.unsplash.com/photo-1743756618181-99a77edf8eab?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8" alt="">
+        <img src="https://images.unsplash.com/photo-1743756618181-99a77edf8eab?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8" alt="">
+    </div>
+    <div class="profile-context"></div>
+    <div class="profile-content"></div>
+</div>
